e472dbca5aed3c55cdc4b6289f959dcd055f6759
Hua Peng <huap@vmware.com>
2018-09-10 17:13:48 +0800

    Good enough with strings.Title now

--- Bandname/Bandname.go
+++ Bandname/Bandname.go
@@ -10,7 +10,7 @@ func BandNameGenerator(word string) string {
 	reg := regexp.MustCompile(`(\w)(\S*)(\w)`)
 	result := reg.FindStringSubmatch(word)
 	if result[1] == result[3] {
-		return strings.ToUpper(result[1]) + result[2] + result[3] + result[2] + result[3]
+		return strings.Title(result[1] + result[2] + result[3] + result[2] + result[3])
 	} else {
 		return "The " + strings.Title(word)
 	}
