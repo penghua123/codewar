a5840c2a396353264699e302b9abea0c1c1770a0
Hua Peng <huap@vmware.com>
2018-09-10 17:12:11 +0800

    Fix the issue with step-daughter but still not good enough

--- Bandname/Bandname.go
+++ Bandname/Bandname.go
@@ -12,7 +12,7 @@ func BandNameGenerator(word string) string {
 	if result[1] == result[3] {
 		return strings.ToUpper(result[1]) + result[2] + result[3] + result[2] + result[3]
 	} else {
-		return "The " + strings.ToUpper(result[1]) + result[2] + result[3]
+		return "The " + strings.Title(word)
 	}
 	return "Error! There are non character in the string"
 }
