0da485b76333df4b25bb33f572b12524659bf9fe
Hua Peng <huap@vmware.com>
2018-09-28 16:12:28 +0800

    A better algorithm to realize

--- pianoL2/main.go
+++ pianoL2/main.go
@@ -5,7 +5,10 @@ import (
 )
 
 func main() {
-	key := WhichNote(101)
+	keyPressCount := 2017
+	key := WhichNote(keyPressCount)
+	fmt.Println(key)
+	key = WhichNoteBetter(keyPressCount)
 	fmt.Println(key)
 }
 
@@ -16,5 +19,11 @@ func WhichNote(keyPressCount int) string {
 	}
 	keyType := []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
 	key := keyType[(keyRealCount-1)%len(keyType)]
+	return key
+}
+
+func WhichNoteBetter(keyPressCount int) string {
+	keyType := []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
+	key := keyType[(keyPressCount-1)%88%len(keyType)]
 	return key
 }
